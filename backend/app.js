6b96e871b0a777c282ceef667e88da5ad730adae
Codruta Craiovanu <codruta.craiovanu@yahoo.com>
2022-04-05 19:04:16 +0300

    feat: create routes for /users

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express();
 const port = 3001;
 
+const user = require('./src/api/users/users')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-});
+})
+
+app.use("/users", user)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
-});
+})
---
+++ backend/src/api/users/users.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router()
+
+router.route("/new").post((req,res =>{
+    res.send({status: 200, message: 'Everything seems to be working fine on POST'})
+}))
+
+router.route("/all").get((req,res =>{
+    res.send({status: 200, message: 'Everything seems to be working fine on GET'})
+}))
+
+router.route("/search").get((req,res =>{
+    res.send({status: 200, message: 'Everything seems to be working fine on GET'})
+}))
