65bbffe0087238250327126d13aabd1424493d8b
Codruta Craiovanu <codruta.craiovanu@yahoo.com>
2022-04-05 19:20:01 +0300

    feat: add contact route

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const port = 3001;
 const user = require('./src/api/users/users')
 const design = require('./src/api/designs/designs')
 const login = require('./src/api/login/login')
+const contact = require('./src/api/contact/contact')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
 app.use("/users", user)
 app.use("/designs", design)
 app.use("/login", login)
+app.use("/contact", contact)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
