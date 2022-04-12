b83b69b0dae328d5586bdd92469d0b79ef2a5813
Bran Denisa <biancadenisa93@yahoo.com>
2022-04-12 18:28:10 +0300

    feat: add headerComponent

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './Header.css'
+
+
+const Header = (props) => {
+
+    return (
+        <div className='header'>
+        <div  className='headerTitle'>
+            BeautifulInteriors
+        </div>
+        <div className='headerDescriprion'>
+            Insert some text here
+        </div>
+    </div>
+    )
+}
+
+export default Header
