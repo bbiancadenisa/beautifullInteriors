304f95201b1437b4d58ee564fcd77d04f2ff0329
Codruta Craiovanu <codruta.craiovanu@yahoo.com>
2022-05-16 02:16:13 +0300

    feat: get all designs from one user

--- frontend/src/components/Designs/Design.js
+++ frontend/src/components/Designs/Design.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
 import Product from '../Product/Product'
 
 function Design() {
+
+  const userId = localStorage.getItem('id')
+  const [designs, setDesigns] = useState([])
+
+  const getAllDesign = async (e) => {
+      try {
+          let res = await axios.get(`http://localhost:3001/designs/all/${userId}`)
+          console.log(res.data.designs,'res')
+          if(res)
+            setDesigns(res.data.designs)
+      } catch (e) {
+          console.log(e);
+      } 
+  }
+
+useEffect( () => {
+  getAllDesign()
+}, [])
+
   return (
-    <div><Product /></div>
+    <div><Product designs={designs} />
+    </div>
   )
 }
 
