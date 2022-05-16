64272e2a8962d99ba364ddf1f26a2603988decdf
Bran Bianca <biancadenisa93@yahoo.com>
2022-05-16 22:42:30 +0300

    feat: style design list in category

--- frontend/src/components/Category/Category.js
+++ frontend/src/components/Category/Category.js
@@ -1,46 +1,44 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import './Category.css'
 
 function Category() {
 
+    const [designs, setDesigns] = useState([])
     const {category} = useParams()
-    const data = [{
-        name: 'Alicia Delucia',
-        image: '/images/bedroom.jpg'
-    },{
-        name: 'Alicia Delucia',
-        image: '/images/bedroom.jpg'
-    },{
-        name: 'Alicia Delucia',
-        image: '/images/bedroom.jpg'
-    },{
-        name: 'Alicia Delucia',
-        image: '/images/bedroom.jpg'
-    },{
-        name: 'Alicia Delucia',
-        image: '/images/bedroom.jpg'
-    },{
-        name: 'Alicia Delucia',
-        image: '/images/bedroom.jpg'
-    },{
-        name: 'Alicia Delucia',
-        image: '/images/bedroom.jpg'
-    }]
+   
+    const getDesigns = async () => {
+        try {
+            let res = await axios.get(`http://localhost:3001/designs/category/${category.toLowerCase()}`)
+            if(res)
+              setDesigns(res.data)
+        } catch (e) {
+            console.log(e);
+        } 
+    }
+
+    useEffect(() => {
+        getDesigns()
+    }, [])
+
   return (
-    <div>
-        <h1>Designs for {category} </h1>
-        <div className="row">
-            {data.map((design,index) => {
+    
+      <div className='text-white container-fluid d-flex flex-column align-items-center text-align-center containerCategory'>
+        <h1 className='title'>Designs for {category} </h1>
+        <div className="row d-flex">
+            {designs.map((design,index) => {
                 return (
-               <div key={index} className='col-lg-3 d-flex flex-column text-center'>
+               <div key={index} className='col-lg-3 d-flex flex-column text-center align-items-center mb-4'>
                <span className='mb-1'>
-                   {design.name}
+                   {design.title}
                </span>
-               <img className='imgCategory' src={`${design.image}`} alt='' />
+               <img className='imgCategory' src="/images/kitchen.jpg" alt='' />
            </div>)
             })}
         </div>
-    </div>
+        </div>
+    
   )
 }
 
